6f85e3515e2fe221e1d62cf45daf31532660e743
adnanniazi <Raz0r@321>
2026-01-27 15:04:18 +0500

    space removed

--- src/components/home/ToolsSection.tsx
+++ src/components/home/ToolsSection.tsx
@@ -15,8 +15,8 @@ export const ToolsSection = () => {
           ))}
         </div>
       </div>
-      <div className="absolute -bottom-[150%] lg:block hidden z-0 left-0">
-        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="1784" viewBox="0 0 1365 1784" fill="none">
+      <div className="absolute -bottom-[100%] lg:block 5xl:hidden hidden z-0 left-0">
+        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="1284" viewBox="0 0 1365 1784" fill="none">
           <g filter="url(#filter0_f_298_164)">
             <path d="M444.333 861.798C697.293 745.427 670.57 980.243 656.158 1083.67L-43.4536 1073.11C-57.4074 936.004 7.93945 688.347 90.0648 700.426C300.481 731.373 217.925 965.954 444.333 861.798Z" fill="#FFD400" fillOpacity="0.64" />
           </g>
